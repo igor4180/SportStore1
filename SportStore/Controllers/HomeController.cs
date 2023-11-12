03bb866b61e4e16dc72b47f2ad7555912b6c4d47
Igor Beskletkin <Beskletkin@gmail.com>
2023-11-12 23:16:03 +0300

    1

---
+++ SportStore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using SportStore.Models.ViewModels;
+
+
+namespace SportStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		private IStoreRepository repository;
+		public int PageSize = 4;
+		public HomeController(IStoreRepository repo)
+		{
+			repository = repo;
+		}
+
+		public IActionResult Index(string? category, int productPage = 1)
+		{
+			return View(new ProductListViewModel { Products =
+				repository.Products
+				.Where(p=> category == null || p.Category == category)
+				.OrderBy(p => p.ProductID)
+				.Skip((productPage - 1) * PageSize).Take(PageSize),
+				PagingInfo = new PagingInfo
+				{
+					CurrentPage = productPage,
+					ItemsPerPage = PageSize,
+
+					TotalItems = category == null ? repository.Products.Count() : repository.Products
+															.Where(e=>e.Category==category).Count()
+				},
+				CurrentCategory = category
+			});
+
+	}
+
+		
+	}
+}
---
+++ SportStore/Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SportStore.Models.ViewModels
+{
+	public class ProductListViewModel
+	{
+		 public IEnumerable<Product> Products { get; set; }
+			=Enumerable.Empty<Product>();
+		public string? CurrentCategory { get; set; }
+		public PagingInfo PagingInfo { get; set; } = new();
+	}
+}
